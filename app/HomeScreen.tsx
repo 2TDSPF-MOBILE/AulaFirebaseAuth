74b6f1ed12b0be42b455ebb95fa01d3260060f48
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-19 11:56:46 -0300

    Criado o banco de dados em aula

--- app/HomeScreen.tsx
+++ app/HomeScreen.tsx
@@ -1,11 +1,39 @@
-import { Text,Button } from "react-native";
+import { Text,Button,Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 
+import {auth} from '../services/firebaseConfig'
+import { deleteUser } from "firebase/auth";
+
 
 export default function HomeScreen(){
     const router = useRouter()//Hook de navegação
+
+    const excluirConta = ()=>{
+        Alert.alert("CONFIRMAR EXCLUSÃO","Tem certeza que deseja excluir a conta? Esta ação não poderá ser desfeita.",
+            [
+                {text:"Cancelar",style:'cancel'},
+                {text:"Excluir",style:'destructive',
+                    onPress:async()=>{
+                        try{
+                            const user = auth.currentUser
+                            if(user){
+                                await deleteUser(user)
+                                await AsyncStorage.removeItem('@user')
+                                Alert.alert("Conta Excluída","Conta excluída com sucesso.")
+                                router.push('/')
+                            }else{
+                                Alert.alert("Erro","Nenhum usuário logado")
+                            }
+                        }catch(error){
+                            Alert.alert("Erro","Não possível excluir a conta.")
+                        }
+                    }
+                }
+            ],{cancelable:true}//Permite cancelar o alerta
+        )
+    }
 
     const realizarLogoff = async()=>{
         await AsyncStorage.removeItem('@user')
@@ -16,6 +44,8 @@ export default function HomeScreen(){
         <SafeAreaView>
             <Text>Seja bem-vindo - Você está logado!!</Text>
             <Button title="Realizar logoff" onPress={realizarLogoff}/>
+            <Button title="Excluir Conta" color='red' onPress={excluirConta}/>
+            <Button title="Alterar Senha" color='orange' onPress={()=>router.push('/AlterarSenhaScreen')}/>
         </SafeAreaView>
     )
 }
